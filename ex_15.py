bf6e80d95ba6a9b2c572cc9bd0b78c3e15aedc1d
Tomás <tomasarreguib@gmail.com>
2023-04-12 13:57:50 -0300

    ADD: exercise 15

---
+++ ex_15.py
@@ -0,0 +1,28 @@
+'''
+Escribí un programa que solicite al usuario el ingreso de un número 
+mayor que 1 por teclado e informe si el número es primo o no.
+Nota: los números primos son los que son divisible por si mismos
+y por 1
+'''
+
+SHOULD_CONTINUE = True
+
+while SHOULD_CONTINUE:
+    odd_number = int(input('Ingresá un número mayor a 1: '))
+
+    if odd_number == 1:
+        SHOULD_CONTINUE = False
+        print('Cerrando...')
+        continue
+
+    IS_ODD = True
+    for i in range(2, odd_number):
+        if odd_number % 2 == 0:
+            IS_ODD = False
+            break
+
+    if IS_ODD:
+        print(f'{odd_number} es primo')
+
+    else:
+        print(f'{odd_number} no es primo')
