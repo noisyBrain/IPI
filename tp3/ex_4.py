5ebcb1baee49530f832814c0bbfb5d9275f08d4e
Tomás <tomasarreguib@gmail.com>
2023-04-24 15:27:47 -0300

    ADD: exercise 4

---
+++ tp3/ex_4.py
@@ -0,0 +1,7 @@
+from functions import *
+
+input_number = int(input('Ingresá un número entero: '))
+
+result = sum_digits(input_number)
+
+print(f'Esto es la suma total de los dígitos del número que ingresaste: {result}')
--- tp3/functions.py
+++ tp3/functions.py
@@ -5,3 +5,15 @@ almacenará todas las funciones usadas en los demás archivos
 # pylint: disable= missing-function-docstring
 def is_even(number):
     return number % 2 == 0
+
+def sum_digits(number):
+    aux = number
+    sum_of_digits = 0
+
+    while aux > 0:
+        rest = aux % 10
+        sum_of_digits += rest
+
+        aux = aux // 10
+
+    return sum_of_digits
