48a07d746b2961b29d5735cd6a80180857308dfa
Tomás <tomasarreguib@gmail.com>
2023-12-01 17:53:50 -0300

    refactor(tp2-ex9): changed upper case to lower case

--- tp2/dir_ex_9/ex_9.py
+++ tp2/dir_ex_9/ex_9.py
@@ -1,12 +1,13 @@
 '''
-Modificar el programa anterior para que, si se ingresa un número negativo, 
-no se sume pero continúe con el proceso. 
-Finalmente, mostrar por separado la suma de los números positivos pares e impares ingresados.
+Modificar el programa anterior para que, si se ingresa un número negativo,
+no se sume pero continúe con el proceso.
+Finalmente, mostrar por separado la suma de los números positivos pares e
+impares ingresados.
 '''
 amount_of_numbers = int(input('Qué cantidad de números vas a querer ingresar? '))
 
-EVEN_NUMBER_RESULT = 0
-ODD_NUMBER_RESULT = 0
+even_number_result = 0
+odd_number_result = 0
 
 while amount_of_numbers > 0:
     number = int(input('Ingresá los números: '))
@@ -14,12 +15,12 @@ while amount_of_numbers > 0:
         amount_of_numbers -= 1
 
     if number % 2 == 0:
-        EVEN_NUMBER_RESULT += number
+        even_number_result += number
 
     else:
-        ODD_NUMBER_RESULT += number
+        odd_number_result += number
 
     amount_of_numbers -= 1
 
 
-print(f'Even number result: {EVEN_NUMBER_RESULT}\nOdd number result: {ODD_NUMBER_RESULT}')
+print(f'Even number result: {even_number_result}\nOdd number result: {odd_number_result}')
