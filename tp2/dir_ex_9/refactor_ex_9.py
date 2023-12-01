66b6e1ef75c25f12985631c7165744a7a9204457
Tomás <tomasarreguib@gmail.com>
2023-12-01 18:24:55 -0300

    refactor(tp2-ex9): first refacto

---
+++ tp2/dir_ex_9/refactor_ex_9.py
@@ -0,0 +1,11 @@
+'''
+Modificar el programa anterior para que, si se ingresa un número negativo,
+no se sume pero continúe con el proceso.
+Finalmente, mostrar por separado la suma de los números positivos pares e
+impares ingresados.
+'''
+
+
+def total_numbers_to_process():
+    amount_of_numbers = int(input('Qué cantidad de números vas a querer ingresar? '))
+    return amount_of_numbers
