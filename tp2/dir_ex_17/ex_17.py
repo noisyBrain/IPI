3162ac3f1f67bb5e10b0958cd9ad078619cc0c3a
Tomás <tomasarreguib@gmail.com>
2023-12-01 11:50:29 -0300

    refactor(ex17)

--- tp2/dir_ex_17/ex_17.py
+++ tp2/dir_ex_17/ex_17.py
@@ -10,17 +10,17 @@ cadena).
 
 str_input = input('Ingresá un texto para analizar: ').strip()
 
-WORDS_IN_INPUT = 0
-WORD = ''
-LONGEST_WORD = ''
+words_in_input = 0
+word = ''
+longest_word = ''
 
 while ' ' in str_input:
-    WORD = WORD[:WORD.find(' ')]
-    WORDS_IN_INPUT += 1
+    word = word[:word.find(' ')]
+    words_in_input += 1
 
-    print(f'Longitud de {WORD}: {len(WORD)}')
+    print(f'Longitud de {word}: {len(word)}')
 
-    if len(WORD) > len(LONGEST_WORD):
-        LONGEST_WORD = WORD
-    
+    if len(word) > len(longest_word):
+        longest_word = word
+
     str_input = str_input[str_input.find(' '):]
