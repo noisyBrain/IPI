d1e716384632dc882a6d56495013335ea841f060
Tomás <tomasarreguib@gmail.com>
2023-12-01 11:49:50 -0300

    refactor(ex17): deleted old commented code

--- tp2/dir_ex_17/ex_17.py
+++ tp2/dir_ex_17/ex_17.py
@@ -3,30 +3,10 @@ Escribí un programa que solicite el ingreso de una cadena de texto e informe:
 a) La cantidad de palabras que posee la cadena.
 b) La cantidad de caracteres que posee cada palabra.
 c) La palabra más larga que contiene la cadena.
-Nota: cada palabra estará separada por un espacio en blanco 
+Nota: cada palabra estará separada por un espacio en blanco
 (puede haber espacios al comienzo y al final de la
 cadena).
 '''
-
-# str_input = input('Ingresá un texto para analizar: ').strip()
-# separated_words = str_input.split()
-# words_in_input = len(separated_words)
-#
-# LONGEST_WORD = ''
-#
-# print(f'La cadena "{str_input}" tiene {words_in_input} palabras\n')
-#
-# for word in separated_words:
-#     is_longer = len(word) > len(LONGEST_WORD)
-#
-#     if is_longer:
-#         LONGEST_WORD = word
-#
-#     print(f'La palabra "{word}" tiene: {len(word)} letras\n')
-#
-# print(f'La palabra más larga de "{str_input}" es "{LONGEST_WORD}"\n')
-
-# Hecho con find y slices:
 
 str_input = input('Ingresá un texto para analizar: ').strip()
 
