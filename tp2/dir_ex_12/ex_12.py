7a5447c27db92d0fdea4df54010f5024a64cab68
Tomás <tomasarreguib@gmail.com>
2023-12-04 17:55:43 -0300

    refactor(tp2-ex12): changed upper case to lower case in order to improve code styling

--- tp2/dir_ex_12/ex_12.py
+++ tp2/dir_ex_12/ex_12.py
@@ -4,21 +4,22 @@ en una tienda durante la última semana. Luego, se deberá mostrar el monto tota
 Si se lee un monto negativo, se debe informar el problema sin interrumpir el ingreso de los datos.
 La lectura de la información finaliza al leer un monto igual a cero
 '''
-SHOULD_CONTINUE = True
-SELLS = 0
+
+should_continue = True
+sells = 0
 
 suma_monto = 0
-monto = int(input("ingrese el monto de venta, ingrese 0 para finalizar"))
+monto = int(input("ingrese el monto de venta, ingrese 0 para finalizar: "))
 
 while monto != 0:
 
     if monto < 0:
         print("ingreso un valor negativo")
-        monto = int(input("ingrese el monto de venta, ingrese 0 para finalizar"))
+        monto = int(input("ingrese el monto de venta, ingrese 0 para finalizar: "))
         continue
 
-    elif monto > 0: 
+    else: 
         suma_monto += monto
-        monto = int(input("ingrese el monto de venta, ingrese 0 para finalizar"))
+        monto = int(input("ingrese el monto de venta, ingrese 0 para finalizar: "))
 
-print(suma_monto)
+print(f"\nLa suma total de las ventas es de: {suma_monto}")
